e6277df18fe398b989502dc6c5b79952c3b99227
Gautham J <Gauthamastro@gmail.com>
2020-08-18 17:55:57 +0530

    Modified chain_spec.rs: Removed Initial Authorities Reduced endowed_accounts

--- nodes/babe-grandpa-node/src/chain_spec.rs
+++ nodes/babe-grandpa-node/src/chain_spec.rs
@@ -13,7 +13,7 @@ pub type ChainSpec = sc_service::GenericChainSpec<GenesisConfig>;
 /// Build a Development ChainSpec
 pub fn dev_config() -> ChainSpec {
 	ChainSpec::from_genesis(
-		"Development",
+		"DEX Development",
 		"dev",
 		sc_service::ChainType::Development,
 		dev_genesis,
@@ -28,29 +28,29 @@ pub fn dev_config() -> ChainSpec {
 /// Build a Local Chainspec
 pub fn local_testnet_config() -> ChainSpec {
 	ChainSpec::from_genesis(
-		"Local Testnet",
-		"local_testnet",
+		"DEX Local Testnet",
+		"dex_local_testnet",
 		sc_service::ChainType::Local,
 		|| {
 			testnet_genesis(
 				vec![
-					authority_keys_from_seed("Alice"),
-					authority_keys_from_seed("Bob"),
+					// authority_keys_from_seed("Alice"),
+					// authority_keys_from_seed("Bob"),
 				],
 				account_id_from_seed::<sr25519::Public>("Alice"),
 				vec![
 					account_id_from_seed::<sr25519::Public>("Alice"),
 					account_id_from_seed::<sr25519::Public>("Bob"),
-					account_id_from_seed::<sr25519::Public>("Charlie"),
-					account_id_from_seed::<sr25519::Public>("Dave"),
-					account_id_from_seed::<sr25519::Public>("Eve"),
-					account_id_from_seed::<sr25519::Public>("Ferdie"),
-					account_id_from_seed::<sr25519::Public>("Alice//stash"),
-					account_id_from_seed::<sr25519::Public>("Bob//stash"),
-					account_id_from_seed::<sr25519::Public>("Charlie//stash"),
-					account_id_from_seed::<sr25519::Public>("Dave//stash"),
-					account_id_from_seed::<sr25519::Public>("Eve//stash"),
-					account_id_from_seed::<sr25519::Public>("Ferdie//stash"),
+					// account_id_from_seed::<sr25519::Public>("Charlie"),
+					// account_id_from_seed::<sr25519::Public>("Dave"),
+					// account_id_from_seed::<sr25519::Public>("Eve"),
+					// account_id_from_seed::<sr25519::Public>("Ferdie"),
+					// account_id_from_seed::<sr25519::Public>("Alice//stash"),
+					// account_id_from_seed::<sr25519::Public>("Bob//stash"),
+					// account_id_from_seed::<sr25519::Public>("Charlie//stash"),
+					// account_id_from_seed::<sr25519::Public>("Dave//stash"),
+					// account_id_from_seed::<sr25519::Public>("Eve//stash"),
+					// account_id_from_seed::<sr25519::Public>("Ferdie//stash"),
 				],
 			)
 		},
