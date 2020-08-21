a99936cc4c81ef0c75105ce936ef469ef0186b55
Gautham J <Gauthamastro@gmail.com>
2020-08-21 12:25:30 +0530

    Forgot to add the Council pallets to construct_runtime marco

--- runtimes/babe-grandpa-runtime/src/lib.rs
+++ runtimes/babe-grandpa-runtime/src/lib.rs
@@ -495,6 +495,9 @@ construct_runtime!(
 		TransactionPayment: transaction_payment::{Module, Storage},
 		Staking: pallet_staking::{Module, Call, Config<T>, Storage, Event<T>, ValidateUnsigned},
 		Session: pallet_session::{Module, Call, Storage, Event, Config<T>},
+		MainnetCouncil: pallet_collective::<Instance1>::{Module, Call, Storage, Origin<T>, Event<T>, Config<T>},
+		TechnicalCouncil: pallet_collective::<Instance2>::{Module, Call, Storage, Origin<T>, Event<T>, Config<T>},
+		PBTCCouncil: pallet_collective::<Instance3>::{Module, Call, Storage, Origin<T>, Event<T>, Config<T>},
 		// Treasury: pallet_treasury::{Module, Call, Storage, Config, Event<T>}
 		// The Recipe Pallets
 		// (None)
