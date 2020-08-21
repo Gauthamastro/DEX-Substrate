7bc6b9f061938d18bbc908e54387aadf094dace9
Gautham J <Gauthamastro@gmail.com>
2020-08-21 19:56:59 +0530

    Successfully Compiled Runtime & Node with 3 councils (TESTED) PBTC COUNCIL, MAINNET COUNCIL, TECHNICAL COUNCIL

--- start-observer.sh
+++ start-observer.sh
@@ -1,1 +1,1 @@
-./target/debug/babe-grandpa-node --port 30334 --ws-port 9000 --bootnodes /ip4/15.207.157.197/tcp/30334/p2p/12D3KooWQT7ngySk8nbeZRvENxCM3uBmyUGM4MRearS5QwLFbYAq
+./target/debug/babe-grandpa-node --dave --chain=customSpecRaw.json --port 30334 --ws-port 9000 --bootnodes /ip4/15.207.157.197/tcp/30334/p2p/12D3KooWEyH3s9xxMmxLA3bpzz6LANXEAzC62d5ocFP6LBwxTYeG
